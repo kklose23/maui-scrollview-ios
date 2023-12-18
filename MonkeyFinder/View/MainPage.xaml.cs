9f93c400c552113e2f8911804c398dd0b9c32dae
Kevin Klose <kevin.klose@teamdynamix.com>
2023-12-18 13:53:34 -0500

    first commit

---
+++ MonkeyFinder/View/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿namespace MonkeyFinder.View;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage(MonkeysViewModel viewModel)
+	{
+		InitializeComponent();
+		BindingContext = viewModel;
+	}
+
+
+  public static readonly BindableProperty ShowProperty = BindableProperty.Create(nameof(Show), typeof(bool), typeof(MainPage), defaultValue: true);
+
+  public bool Show
+  {
+    get => (bool)GetValue(ShowProperty);
+    set => SetValue(ShowProperty, value);
+  }
+
+  private void Button_Clicked(object sender, EventArgs e)
+  {
+    Show = false;
+  }
+}
+
+
+
---
+++ MonkeyFinder/ViewModel/MonkeysViewModel.cs
@@ -0,0 +1,32 @@
+﻿using MonkeyFinder.Services;
+
+namespace MonkeyFinder.ViewModel;
+
+public partial class MonkeysViewModel : BaseViewModel
+{
+  public MonkeysViewModel()
+  {
+    Items = new List<string>()
+    {
+      "item with long name 1",
+      "item with long name 2",
+      "item with long name 3",
+      "item with long name 4",
+    };
+  }
+
+  [ObservableProperty]
+  private IEnumerable<string> _items;
+
+  [RelayCommand]
+  async Task GoToDetails(Monkey monkey)
+  {
+    if (monkey == null)
+      return;
+
+    await Shell.Current.GoToAsync(nameof(DetailsPage), true, new Dictionary<string, object>
+    {
+        {"Monkey", monkey }
+    });
+  }
+}
